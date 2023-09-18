a58c997457215c0c6f47dfe4acbdfc83f30966b3
0MoonSon0 <145151201+0MoonSon0@users.noreply.github.com>
2023-09-18 17:18:43 +0500

    Update Program.cs

--- PZ_02/Program.cs
+++ PZ_02/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+namespace PZ_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите значение a: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите значение c: ");
+            double c = Convert.ToDouble(Console.ReadLine());
+            double x, y, z;
+            double result;
+
+            if (a > Math.PI)
+            {
+                x = Math.Cos(a + 2 * c);
+            }
+            else
+            {
+                x = c + Math.Sqrt(a * (c * c) - 2 * (a + Math.PI));
+            }
+
+            if (x <= 0)
+            {
+                y = Math.Log(a + c - x);
+            }
+            else
+            {
+                y = Math.Sin(2 * a * x) + Math.Sin(a + x);
+            }
+
+            z = (2 * x + 3 * y) * (a * a - c * c);
+
+            Console.WriteLine("x = " + x);
+            Console.WriteLine("y = " + y);
+            Console.WriteLine("z = " + z);
+
+            Console.ReadLine();
+        }
+    }
+}
