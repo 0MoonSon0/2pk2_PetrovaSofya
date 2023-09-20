3f9a7160a097ad9717b6e64d2ba38643e986b6ac
Софья Сергеевна Петрова <petrova.ss2217@oksei.onmicrosoft.com>
2023-09-20 22:59:26 +0500

    3 пз

--- PZ_03/Program.cs
+++ PZ_03/Program.cs
@@ -1,10 +1,40 @@
-﻿namespace PZ_03
+﻿class Program
 {
-    internal class Program
+    static void Main()
     {
-        static void Main(string[] args)
+        Console.WriteLine("Введите тип фигуры (круг, прямоугольник, треугольник):");
+        string shape = Console.ReadLine();
+
+        switch (shape.ToLower())
         {
-            Console.WriteLine("Hello, World!");
+            case "круг":
+                Console.WriteLine("Введите радиус круга:");
+                double radius = double.Parse(Console.ReadLine());
+                double circleArea = Math.PI * radius * radius;
+                Console.WriteLine("Площадь круга: " + circleArea);
+                break;
+
+            case "прямоугольник":
+                Console.WriteLine("Введите ширину прямоугольника:");
+                double width = double.Parse(Console.ReadLine());
+                Console.WriteLine("Введите высоту прямоугольника:");
+                double height = double.Parse(Console.ReadLine());
+                double rectangleArea = width * height;
+                Console.WriteLine("Площадь прямоугольника: " + rectangleArea);
+                break;
+
+            case "треугольник":
+                Console.WriteLine("Введите основание треугольника:");
+                double baseTriangle = double.Parse(Console.ReadLine());
+                Console.WriteLine("Введите высоту треугольника:");
+                double heightTriangle = double.Parse(Console.ReadLine());
+                double triangleArea = 0.5 * baseTriangle * heightTriangle;
+                Console.WriteLine("Площадь треугольника: " + triangleArea);
+                break;
+
+            default:
+                Console.WriteLine("Некорректный тип фигуры.");
+                break;
         }
     }
 }
