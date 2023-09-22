645befdad5a73e1e47187d03767c3f43208282ed
Софья Сергеевна Петрова <petrova.ss2217@oksei.onmicrosoft.com>
2023-09-23 03:47:58 +0500

    5 pz

---
+++ PZ_05/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        double x = 1;
+        double h = 0.2;
+
+        Console.WriteLine("x\t\t y");
+
+        while (x <= 3)
+        {
+            double y = 8 * x + Math.Pow(4 * x, 4) - Math.Pow(1.5 * x, 3);
+            Console.WriteLine($"{x}\t\t {y}");
+
+            x += h;
+        }
+
+        Console.ReadLine();
+    }
+}
+
