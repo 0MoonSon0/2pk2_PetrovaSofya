9b7fae00702eb9b9740eb7876495dab6d408905e
Софья Сергеевна Петрова <petrova.ss2217@oksei.onmicrosoft.com>
2023-09-27 15:42:14 +0500

    добавленна 6 пз

---
+++ PZ_06/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Ввод размера массива
+        Console.Write("Введите размер массива: ");
+        int n = int.Parse(Console.ReadLine());
+
+        // Создание и заполнение массива
+        int[] arr = new int[n];
+        Console.WriteLine("Введите элементы массива:");
+        for (int i = 0; i < n; i++)
+        {
+            arr[i] = int.Parse(Console.ReadLine());
+        }
+
+        // Поиск номера минимального элемента по модулю
+        int minIndex = 0;
+        int minValue = Math.Abs(arr[0]);
+        for (int i = 1; i < n; i++)
+        {
+            if (Math.Abs(arr[i]) < minValue)
+            {
+                minValue = Math.Abs(arr[i]);
+                minIndex = i;
+            }
+        }
+
+        // Вывод результата
+        Console.WriteLine("Номер минимального по модулю элемента: " + minIndex);
+    }
+}
