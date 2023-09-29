79406849e047eacc4f285518a54e594de4adb1ea
Софья Сергеевна Петрова <petrova.ss2217@oksei.onmicrosoft.com>
2023-09-29 13:56:02 +0500

    добавлена 7 пз

---
+++ PZ_07/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Создаем квадратную матрицу 3x3
+        int[,] matrix = new int[3, 3] { { 1, 0, 2 }, { 3, 4, 5 }, { 6, 7, 0 } };
+
+        // Находим размерность квадратной матрицы
+        int size = (int)Math.Sqrt(matrix.Length);
+
+        // Инициализируем произведение единицей
+        int product = 1;
+
+        // Проходим по главной диагонали и умножаем ненулевые элементы
+        for (int i = 0; i < size; i++)
+        {
+            if (matrix[i, i] != 0)
+            {
+                product *= matrix[i, i];
+            }
+        }
+
+        // Выводим результат
+        Console.WriteLine("Произведение ненулевых диагональных элементов: " + product);
+    }
+}
