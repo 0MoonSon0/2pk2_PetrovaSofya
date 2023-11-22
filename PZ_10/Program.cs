b8e251a633a46f3448c3fa89074d89312b55d5ec
Софья Сергеевна Петрова <petrova.ss2217@oksei.onmicrosoft.com>
2023-11-23 01:09:14 +0500

    пз 10

---
+++ PZ_10/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string text = "";
+
+        while (true)
+        {
+            string sentence = Console.ReadLine();
+            if (sentence == "exit")
+                break;
+
+            text += sentence + " ";
+        }
+
+        Console.WriteLine(text);
+    }
+}
