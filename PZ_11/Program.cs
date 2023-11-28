dcef74b3c6c050193e40c31d968733a52515726c
Софья Сергеевна Петрова <petrova.ss2217@oksei.onmicrosoft.com>
2023-11-28 17:39:18 +0500

    доюавлены 11 и 12 практика

---
+++ PZ_11/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace PZ_11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            void SortDec3(ref double A, ref double B, ref double C)
+            {
+                if (A < B)
+                {
+                    double temp = A;
+                    A = B;
+                    B = temp;
+                }
+
+                if (B < C)
+                {
+                    double temp = B;
+                    B = C;
+                    C = temp;
+                }
+
+                if (A < B)
+                {
+                    double temp = A;
+                    A = B;
+                    B = temp;
+                }
+            }
+
+            // Пример использования SortDec3 для упорядочивания двух наборов чисел
+            double A1 = 5.2, B1 = 3.7, C1 = 1.9;
+            double A2 = 4.8, B2 = 6.1, C2 = 2.5;
+
+            SortDec3(ref A1, ref B1, ref C1);
+            SortDec3(ref A2, ref B2, ref C2);
+
+            Console.WriteLine($"Первый набор чисел: {A1}, {B1}, {C1}");
+            Console.WriteLine($"Второй набор чисел: {A2}, {B2}, {C2}");
+        }
+    }
+}
---
+++ PZ_12/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace PZ_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            {
+                Console.WriteLine("Введите адрес электронной почты:");
+                string email = Console.ReadLine();
+
+                bool isValidEmail = IsValidEmail(email);
+                Console.WriteLine(isValidEmail);
+            }
+
+            static bool IsValidEmail(string email)
+            {
+                try
+                {
+                    var addr = new System.Net.Mail.MailAddress(email);
+                    return addr.Address == email;
+                }
+                catch
+                {
+                    return false;
+                }
+            }
+        }
+    }
+}
